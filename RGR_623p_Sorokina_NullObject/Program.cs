f5d2c7e658de06ae020463de1e8b57b6f8559b4a
AnnaSorokina20 <a.v.sorokina@student.khai.edu>
2024-05-30 23:29:11 +0200

    NullObject

---
+++ RGR_623p_Sorokina_NullObject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+// Абстрактний об'єкт
+public abstract class AbstractObject
+{
+    public abstract void Request();
+}
+
+// Реальний об'єкт
+public class RealObject : AbstractObject
+{
+    public override void Request()
+    {
+        Console.WriteLine("RealObject Request");
+    }
+}
+
+// Null-об'єкт
+public class NullObject : AbstractObject
+{
+    public override void Request()
+    {
+        // Нічого не робить
+    }
+}
+
+// Клієнтський код
+class Client
+{
+    private AbstractObject obj;
+
+    public Client(AbstractObject obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Request();
+    }
+}
+
+// Приклад використання
+class Program
+{
+    static void Main(string[] args)
+    {
+        AbstractObject realObject = new RealObject();
+        AbstractObject nullObject = new NullObject();
+
+        Client client1 = new Client(realObject);
+        Client client2 = new Client(nullObject);
+
+        client1.Execute();
+        client2.Execute();
+    }
+}
