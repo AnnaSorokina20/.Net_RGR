86bf632d73bff4ce6fb64ce095736f45754224ce
AnnaSorokina20 <a.v.sorokina@student.khai.edu>
2024-05-31 00:24:18 +0200

    Update

--- RGR_623p_Sorokina_Lock/Program.cs
+++ RGR_623p_Sorokina_Lock/Program.cs
@@ -1,32 +1,38 @@
 ﻿using System;
 using System.Threading;
 
+// Клас Lock, який забезпечує механізм блокування та розблокування доступу до ресурсу
 public class Lock
 {
     private readonly object _lockObject = new object();
 
+    // Метод для отримання замка
     public void Acquire()
     {
         Monitor.Enter(_lockObject);
     }
 
+    // Метод для звільнення замка
     public void Release()
     {
         Monitor.Exit(_lockObject);
     }
 }
 
+// Клієнтський клас, який використовує замок для доступу до ресурсу
 public class Client
 {
     private Lock _lock;
     private string _name;
 
+    // Конструктор приймає замок і ім'я клієнта
     public Client(Lock lockObject, string name)
     {
         _lock = lockObject;
         _name = name;
     }
 
+    // Метод для доступу до ресурсу
     public void AccessResource()
     {
         Console.WriteLine($"{_name} is trying to acquire the lock.");
@@ -53,15 +59,19 @@ class Program
     {
         Lock lockObject = new Lock();
 
+        // Створюємо клієнтів, які використовують замок
         Client client1 = new Client(lockObject, "Client 1");
         Client client2 = new Client(lockObject, "Client 2");
 
+        // Створюємо потоки для доступу до ресурсу
         Thread thread1 = new Thread(new ThreadStart(client1.AccessResource));
         Thread thread2 = new Thread(new ThreadStart(client2.AccessResource));
 
+        // Запускаємо потоки
         thread1.Start();
         thread2.Start();
 
+        // Чекаємо завершення потоків
         thread1.Join();
         thread2.Join();
     }
--- RGR_623p_Sorokina_NullObject/Program.cs
+++ RGR_623p_Sorokina_NullObject/Program.cs
@@ -1,39 +1,43 @@
 ﻿using System;
 
-// Абстрактний об'єкт
+// Абстрактний об'єкт, який визначає загальний інтерфейс для всіх об'єктів
 public abstract class AbstractObject
 {
     public abstract void Request();
 }
 
-// Реальний об'єкт
+// Реальний об'єкт, який виконує реальні дії
 public class RealObject : AbstractObject
 {
+    // Метод Request виконує реальні дії
     public override void Request()
     {
         Console.WriteLine("RealObject Request");
     }
 }
 
-// Null-об'єкт
+// Null-об'єкт, який не виконує жодних дій
 public class NullObject : AbstractObject
 {
+    // Метод Request не виконує жодних дій
     public override void Request()
     {
         // Нічого не робить
     }
 }
 
-// Клієнтський код
+// Клієнтський клас, який використовує об'єкти AbstractObject без перевірок на null
 class Client
 {
     private AbstractObject obj;
 
+    // Конструктор приймає об'єкт AbstractObject
     public Client(AbstractObject obj)
     {
         this.obj = obj;
     }
 
+    // Метод Execute викликає метод Request об'єкта
     public void Execute()
     {
         obj.Request();
@@ -45,12 +49,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Створюємо реальний об'єкт
         AbstractObject realObject = new RealObject();
+        // Створюємо null-об'єкт
         AbstractObject nullObject = new NullObject();
 
+        // Створюємо клієнтів, які використовують ці об'єкти
         Client client1 = new Client(realObject);
         Client client2 = new Client(nullObject);
 
+        // Виконуємо дії
         client1.Execute();
         client2.Execute();
     }
